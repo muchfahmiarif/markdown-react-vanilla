ff0ea65eda47878254f684444fd6b39313bc8a2a
muchfahmiarif <fahmiarif96@gmail.com>
2023-01-09 10:37:24 +0700

    add post component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import Post from "./components/post/Post";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Post />
     </div>
   );
 }
---
+++ src/components/post/Post.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Post = () => {
+  return (
+    <article className="article">
+      <div class="container">
+        <div class="post-wrapper">
+          <h1>Posts</h1>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default Post;
